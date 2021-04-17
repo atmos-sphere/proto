53b4ecf3c59d8599f67fa8c60b9842ab537f447e
obonobo <ethanbnb@gmail.com>
2021-04-16 21:19:48 -0400

    TEMP: Server-Side Rendering integration with material-ui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,31 @@
+import Head from "next/head";
 import "./app.global.scss";
+import PropTypes from "prop-types";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  // Remove the server-side injected CSS.
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) jssStyles.parentElement.removeChild(jssStyles);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Atmos</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cairo&family=Rubik:wght@500&display=swap" />
+      </Head>
+      <Component {...pageProps} />
+      <footer style={{ display: "none" }}>Atmos Sphere</footer>
+    </>
+  );
 }
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,6 @@
-import Head from "next/head";
 import { AudioPlayer, defaultTracks } from "../src/components/AudioPlayer";
 import Overlay from "../src/components/overlay/Overlay";
 import styles from "./index.module.scss";
-
-const HeaderWithFonts = () => (
-  <Head>
-    <title>Atmos</title>
-    <link rel="icon" href="/favicon.ico" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Cairo&display=swap"
-    />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Cairo&family=Rubik:wght@500&display=swap"
-      rel="stylesheet"
-    />
-  </Head>
-);
 
 const DemoVid = () => <video controls src="/animals.mp4" type="video/mp4" />;
 
@@ -32,13 +14,11 @@ const Content = () => (
 export default function Home() {
   return (
     <div className={styles.container}>
-      <HeaderWithFonts />
       <main>
         <Overlay timeout={2000} />
         <Content />
         <AudioPlayer tracks={defaultTracks} />
       </main>
-      <footer style={{ display: "none" }}>Atmos Sphere</footer>
     </div>
   );
 }
